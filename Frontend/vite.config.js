8788c48e410e6131c9d0bae21be29d32135dea58
PVAGR <pvaglobalreach@gmail.com>
2025-09-07 20:58:44 -0700

    uyyu

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  root: './',
   build: {
-    outDir: '../build',
-    emptyOutDir: true,
-    sourcemap: true
+    outDir: 'dist',
+    emptyOutDir: true
   },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
+  server: {
+    port: 3000
   }
 });
